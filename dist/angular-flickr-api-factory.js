21fb1d1e42f843dda96bfac0d6f3a8c0fec7349b
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-01-06 16:22:35 +0100

    chore: create non-minified dist file (#1)

--- dist/angular-flickr-api-factory.js
+++ dist/angular-flickr-api-factory.js
@@ -1,3 +1,10 @@
+/**
+    @name: angular-flickr-api-factory 
+    @version: 0.5.0 (06-01-2016) 
+    @author: Jonathan Hornung 
+    @url: https://github.com/JohnnyTheTank/angular-flickr-api-factory#readme 
+    @license: MIT
+*/
 "use strict";
 
 angular.module("jtt_flickr", [])
